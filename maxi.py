e305f203037d26c553d3b976647da696f0539754
Shaik485 <Showkathdracula123@gmail.com>
2025-07-01 11:23:56 +0530

    Update maxi.py

--- maxi.py
+++ maxi.py
@@ -1,6 +1,6 @@
-a=[1,2,1,2,1,4,5,6,7,73,7,8,8,2,2,1]
-maxi=a[0]
+a=[1,2,1,2,1,4,5,6,7,73,7,8,8,2,3,3,4,2,2,1]
+sammhy=a[0]
 for i in a:
-    if i>maxi:
-        maxi=i
-print(maxi)
+    if i>sammy:
+        sammy=i
+print(sammy)
